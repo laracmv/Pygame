c09b1bf28421d41c297cf2b5c801d8b4ea659793
Lara Melo <cmvlara1@gmail.com>
2023-11-16 09:24:00 -0300

    jogador 2 LARA

--- assets.py
+++ assets.py
@@ -1,8 +1,10 @@
 import pygame
-from configuracoes import JOGADOR1_ALTURA, JOGADOR1_LARGURA
+from configuracoes import JOGADOR1_ALTURA, JOGADOR1_LARGURA, JOGADOR2_ALTURA, JOGADOR2_LARGURA
 
 def load_assets():
         assets = {}
-        assets['jogador'] = pygame.image.load("assets//img//lutador1.png").convert_alpha()
-        assets['jogador'] = pygame.transform.scale(assets['jogador'], (JOGADOR1_LARGURA, JOGADOR1_ALTURA))
+        assets['jogador1'] = pygame.image.load("assets//img//lutador1.png").convert_alpha()
+        assets['jogador1'] = pygame.transform.scale(assets['jogador1'], (JOGADOR1_LARGURA, JOGADOR1_ALTURA))
+        assets['jogador2'] = pygame.image.load("assets//img//lutador2.png").convert_alpha()
+        assets['jogador2'] = pygame.transform.scale(assets['jogador2'], (JOGADOR2_LARGURA, JOGADOR2_ALTURA))
         return assets
--- tela_de_jogo.py
+++ tela_de_jogo.py
@@ -11,20 +11,64 @@ def tela_de_jogo(tela):
     PULANDO = 1
     CAINDO = 2
 
-    class Jogador(pygame.sprite.Sprite):
+    class Jogador1(pygame.sprite.Sprite):
         def __init__(self, groups, assets):
             # Construtor da classe mãe
             pygame.sprite.Sprite.__init__(self)
 
-            self.image = assets['jogador']
+            self.image = assets['jogador1']
             self.rect = self.image.get_rect()
-            self.rect.centerx = LARGURA / 2
+            self.rect.centerx = LARGURA / 4
             self.rect.bottom = ALTURA - 10
             self.speedx = 0
             self.speedy = 0
             self.groups = groups
             self.assets = assets
 
+            # Usado para decicir se o jogador pode ou não pular
+            self.state = PARADO
+
+        # Esse metodo atualiza a posição do personagem
+        def update(self):
+            # Atualiza a movimentação no eixo x
+            self.rect.x += self.speedx
+            self.speedy += ALTURA_PULO
+            if self.rect.y > 0:
+                self.state = CAINDO
+            self.rect.y += self.speedy
+
+            # Manter dentro da tela
+            if self.rect.right > LARGURA:
+                self.rect.right = LARGURA
+            if self.rect.left < 0:
+                self.rect.left = 0
+            if self.rect.bottom > ALTURA:
+                self.rect.bottom = ALTURA
+                self.speedy = 0
+                self.state = PARADO
+            if self.rect.top < 0:
+                self.rect.top = 0
+            
+        def pulo(self):
+            if self.state == PARADO:
+                self.speedy -= VEL_PULO
+                self.state = PULANDO
+    
+    class Jogador2(pygame.sprite.Sprite):
+        def __init__(self, groups, assets):
+            # Construtor da classe mãe
+            pygame.sprite.Sprite.__init__(self)
+
+            self.image = assets['jogador2']
+            self.rect = self.image.get_rect()
+            self.rect.centerx = LARGURA / 1.5
+            self.rect.bottom = ALTURA - 10
+            self.speedx = 0
+            self.speedy = 0
+            self.groups = groups
+            self.assets = assets
+
+            # Usado para decicir se o jogador pode ou não pular
             self.state = PARADO
 
         # Esse metodo atualiza a posição do personagem
@@ -65,8 +109,10 @@ def tela_de_jogo(tela):
     groups['all_sprites'] = all_sprites
 
     #Criar jogadores 
-    Jogador1 = Jogador(groups, assets)
-    all_sprites.add(Jogador1)
+    jogador1 = Jogador1(groups, assets)
+    jogador2 = Jogador2(groups, assets)
+    all_sprites.add(jogador1)
+    all_sprites.add(jogador2)
 
     MORTO = 0
     JOGANDO = 1
@@ -84,21 +130,32 @@ def tela_de_jogo(tela):
             if event.type == pygame.QUIT:
                 state = MORTO
             if state == JOGANDO:
-                # Teclas jogador 1
+                
                 if event.type == pygame.KEYDOWN:
                     tecla_precionada[event.key] = True
+                    # Teclas jogador 1
                     if event.key == pygame.K_LEFT:
-                        Jogador1.speedx -= 8
+                        jogador1.speedx -= 8
                     if event.key == pygame.K_RIGHT:
-                        Jogador1.speedx += 8
+                        jogador1.speedx += 8
                     if event.key == pygame.K_UP:
-                            Jogador1.pulo() 
+                        jogador1.pulo() 
+                    if event.key == pygame.K_a:
+                        jogador2.speedx -=8
+                    if event.key == pygame.K_d:
+                        jogador2.speedx +=8
+                    if event.key == pygame.K_w:
+                        jogador2.pulo()
                 if event.type == pygame.KEYUP:
                     if event.key in tecla_precionada and tecla_precionada[event.key]:
                         if event.key == pygame.K_LEFT:
-                            Jogador1.speedx += 8
+                            jogador1.speedx += 8
                         if event.key == pygame.K_RIGHT:
-                            Jogador1.speedx -= 8
+                            jogador1.speedx -= 8
+                        if event.key == pygame.K_a:
+                            jogador2.speedx +=8
+                        if event.key == pygame.K_d:
+                            jogador2.speedx -=8
         
         all_sprites.update()
         
