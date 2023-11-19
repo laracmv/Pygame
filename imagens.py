8d2c099dd35bf709d0dbc61036fd8c298674e8de
Láisa Camilly <laisaco1@al.insper.edu.br>
2023-11-18 21:05:58 -0300

    teste de imagens para o início LÁISA

---
+++ imagens.py
@@ -0,0 +1,13 @@
+import pygame
+
+#cada imagem que você importar you create a new surface
+
+#tela inicio
+
+#sky surface
+pygame.image.load('C:\\Users\\laisa\\OneDrive\\Área de Trabalho\\imagem_tela_inicio.webp')
+
+#ground
+pygame.image.load('C:\\Users\\laisa\\OneDrive\\Área de Trabalho\\ground.jpg')
+
+
