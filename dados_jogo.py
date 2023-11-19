3f2a516d278fffef0fd9bf34b1cee7cc1bfeba7c
Láisa Camilly <laisaco1@al.insper.edu.br>
2023-11-18 21:07:29 -0300

    dados do jogo LÁISA

---
+++ dados_jogo.py
@@ -0,0 +1,31 @@
+# Dados do jogo
+LARGURA = 1900
+ALTURA = 959
+FPS = 60
+
+#Tamanhos
+JOGADOR1_ALTURA = 228
+JOGADOR1_LARGURA = 300
+JOGADOR2_ALTURA = 228
+JOGADOR2_LARGURA = 300
+
+#Cores
+PRETO = (0, 0, 0)
+
+# Estados do jogo
+INICIAR = 0
+JOGO = 1
+FIM = 2
+
+#Largura e altura da Tela Principal
+largura = 800
+altura = 400
+
+#Sky Surface
+l = 100
+a = 200
+posk = (0,0)
+branco = (255,255,255)
+
+#Ground Surface
+posg = (0,385) #primeiro é lado e o segundo e altura
