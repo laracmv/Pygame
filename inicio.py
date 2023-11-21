df40bfff60eebd0cd1492362b09673a5b268b5e5
Láisa Camilly <laisaco1@al.insper.edu.br>
2023-11-20 21:39:07 -0300

    botão jogar LÁISA

--- inicio.py
+++ inicio.py
@@ -4,32 +4,48 @@ from dados_jogo import *
 
 class Botao:
     def __init__(self, texto, largura, altura, posicao):
+        #atributos principais
+        self.pressed = False
+
         # retangulo de cima (top)
-        self.retangulo_superior = pygame.Rect(posicao, (largura, altura))
-        self.cor_superior = cor_inicio
+        self.retangulo_cima = pygame.Rect(posicao, (largura, altura))
+        self.cor_cima = cor_inicio
 
         # fonte do texto
         self.fonte = pygame.font.Font(None, 30)
 
         # Texto
-        self.superficie_texto = self.fonte.render(texto, True, black)
-        self.rect_texto = self.superficie_texto.get_rect(center=self.retangulo_superior.center)
+        self.surface_texto = self.fonte.render(texto, True, cor_texto_botao)
+        self.retangulo_texto = self.surface_texto.get_rect(center=self.retangulo_cima.center)
 
     def draw(self):
-        pygame.draw.rect(tela, self.cor_superior, self.retangulo_superior)
-        tela.blit(self.superficie_texto, self.rect_texto)
+        pygame.draw.rect(tela, self.cor_cima, self.retangulo_cima,border_radius = 12) #esse border raius define o raio da borda do retangulo
+        tela.blit(self.surface_texto, self.retangulo_texto)
+        self.clicar()
+    
+
+    def clicar(self):
+        mouse_pos = pygame.mouse.get_pos() #saber a posição do mouse quando for clicar no botão
+        if self.retangulo_cima.collidepoint(mouse_pos): #se nosso mouse ta no nosso botão
+            if pygame.mouse.get_pressed()[0]: #rever
+                self.pressed(True)
+            else:
+                if self.pressed == True:
+                    print('click')
+                    self.pressed = False
+
+
+
 
 # Inicializa o Pygame após as definições de cores
 pygame.init()
-tela = pygame.display.set_mode((500, 500))
+tela = pygame.display.set_mode(tela_inicio)
 pygame.display.set_caption('Início')
 clock = pygame.time.Clock()
 
-# Importa a cor black do arquivo dados_jogo
-black = (0, 0, 0)
 
-# Instancia o botão com os argumentos corretos
-botao1 = Botao('Jogar', 200, 40, (200, 250))
+# argumentos para o botão: O que escreve nele, largura e altura dele, e onde fica.
+botao1 = Botao('Jogar', 200, 40, botao_top)
 
 while True:
     for evento in pygame.event.get():
@@ -37,7 +53,7 @@ while True:
             pygame.quit()
             sys.exit()
 
-    tela.fill(black)
+    tela.fill(fundo_inicio)
     botao1.draw()
 
     # Corrige o erro de digitação na linha a seguir (update em vez de uptade)
