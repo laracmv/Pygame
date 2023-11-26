27325f0a2052c2e1a67b7e1f9dc357061cf6fc12
Lara Melo <cmvlara1@gmail.com>
2023-11-26 14:16:44 -0300

    organizacao LARA

--- animacao.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-def pegaimagem(animacao, largura, altura, escala):
-    imagem = pygame.Surface((largura, altura)).convert_alpha()
-    imagem.blit(animacao, (0,0), (0,0, largura, altura)) #mostra imagem na tela. O 3 argumento pega a porcao da imagem a ser usada
-    imagem = pygame.transform.scale(imagem, (largura * escala, altura * escala))
-    return imagem
--- youtube.py
+++ youtube.py
@@ -6,7 +6,7 @@ galinha_dic = {"idle": [],
                 "run": []
                 }
 
-#para animacao da galinha em idle
+#para animacao da galinha parada
 gidle0 = pygame.image.load("assets//img//galinha//idle//tile000.png")
 galinha_largura = gidle0.get_width() #estabelece largura da galinha
 galinha_altura = gidle0.get_height() #estabelece largura da galinha
@@ -42,6 +42,26 @@ sapo_dic = {"idle": [],
                 "hit": [],
                 "run": []
                 }
+
+#para animacao do sapo parada
+sapoidle0 = pygame.image.load("assets//img//sapo//idle//tile000.png")
+sapo_largura = sapoidle0.get_width() #estabelece largura da galinha
+sapo_altura = sapoidle0.get_height() #estabelece largura da galinha
+sapoidle0= pygame.transform.scale(sapoidle0, (sapo_altura * 4 ,sapo_largura * 4 )) #redimenciona escala sapo
+sapo_dic["idle"].append(sapoidle0) #adiciona no dicionario idle
+
+i = 1
+while i < 11:
+    if i>=1 and i<10:
+        sapoidle = pygame.image.load(f'assets//img//sapo//idle//tile00{i}.png')
+        sapoidle = pygame.transform.scale(gidle , (sapo_altura * 4 ,sapo_altura * 4 ))
+    else:
+        sapoidle = pygame.image.load(f'assets//img//sapo//idle//tile0{i}.png')
+        sapoidle = pygame.transform.scale(gidle , (sapo_altura * 4 ,sapo_altura * 4 ))
+    sapo_dic["idle"].append(gidle)
+    i+=1
+
+#animacao sapo batendo
 
 
 galinha = galinha_dic
